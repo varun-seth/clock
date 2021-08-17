9cd304295b251d3c11dd33155afdc79ed9f24d13
Varun Seth <8343593+varun-seth@users.noreply.github.com>
2021-08-18 01:05:34 +0530

    added legends

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function updateTime() {
-    date = new Date();
-    hours = date.getHours();
-    minutes = date.getMinutes();
-    seconds = date.getSeconds();
-    hour_rotation = 30 * hours + minutes / 2;
-    minute_rotation = 6 * minutes + seconds / 10;
-    second_rotation = 6 * seconds;
+function addLegend(hour) {
+    let element = document.createElement("DIV");
+    element.classList.add("legendsContainer");
+    let innerElement = document.createElement("DIV");
+    element.style.position = "absolute";
+    innerElement.classList.add("legends");
+    let degree = hour * 30;
+    element.style.transform = `translate(50vmin, 5vmin) rotate(${degree}deg)`;
+    clockContainer.appendChild(element);
+    element.appendChild(innerElement);
+}
 
-    hour.style.transform = `translate(-50%, 0%) rotate(${hour_rotation}deg)`;
-    minute.style.transform = `translate(-50%, 0%) rotate(${minute_rotation}deg)`;
-    second.style.transform = `translate(-50%, 0%) rotate(${second_rotation}deg)`;
+for (let i = 0; i < 12; i++) {
+    addLegend(i);
+}
+
+function updateTime() {
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let hourRotation = 30 * hours + minutes / 2 + seconds / 120;
+    let minuteRotation = 6 * minutes + seconds / 10;
+    let secondRotation = 6 * seconds;
+
+    hour.style.transform = `translate(-50%, 0%) rotate(${hourRotation}deg)`;
+    minute.style.transform = `translate(-50%, 0%) rotate(${minuteRotation}deg)`;
+    second.style.transform = `translate(-50%, 0%) rotate(${secondRotation}deg)`;
 }
 setTimeout(() => {
     setInterval(updateTime, 1000);
